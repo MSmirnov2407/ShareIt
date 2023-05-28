eca294298af2c3257c55863bd6045f8eb3c86b83
MSmirnov2407 <msmirnov2407@yandex.ru>
2023-05-28 21:59:16 +0300

    fix: checkstyle

--- src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -18,33 +18,34 @@ public class ItemRequestController {
     private final RequestService requestService;
 
     @Autowired
-    public ItemRequestController(RequestService requestService){
+    public ItemRequestController(RequestService requestService) {
         this.requestService = requestService;
     }
+
     @PostMapping
-    public ItemRequestDto postRequest(@RequestHeader ("X-Sharer-User-Id") int userId,
+    public ItemRequestDto postRequest(@RequestHeader("X-Sharer-User-Id") int userId,
                                       @Valid @RequestBody ItemRequestDto itemRequestDto) {
-        log.info("Создан запрос от пользователя с id={}",userId);
-        return requestService.createRequest(itemRequestDto,userId);
+        log.info("Создан запрос от пользователя с id={}", userId);
+        return requestService.createRequest(itemRequestDto, userId);
     }
 
     @GetMapping
-    public List<ItemRequestDtoWithAnswer> getRequest(@RequestHeader ("X-Sharer-User-Id") int userId) {
-        log.info("Получен список запросов пользователя с id= {}",userId);
+    public List<ItemRequestDtoWithAnswer> getRequest(@RequestHeader("X-Sharer-User-Id") int userId) {
+        log.info("Получен список запросов пользователя с id= {}", userId);
         return requestService.getAllDtoWithAnswByUserId(userId);
     }
 
     @GetMapping("/all")
-    public List<ItemRequestDtoWithAnswer> getAllRequests(@RequestHeader ("X-Sharer-User-Id") int userId,
-                                               @RequestParam (name = "from", defaultValue = "0") int from,
-                                               @RequestParam (name = "size", defaultValue = "3") int size) {
-        log.info("Получены все запросы (одна страница from {} size {})",from,size);
-        return requestService.getAllDtoWithAnsw(from,size, userId);
+    public List<ItemRequestDtoWithAnswer> getAllRequests(@RequestHeader("X-Sharer-User-Id") int userId,
+                                                         @RequestParam(name = "from", defaultValue = "0") int from,
+                                                         @RequestParam(name = "size", defaultValue = "3") int size) {
+        log.info("Получены все запросы (одна страница from {} size {})", from, size);
+        return requestService.getAllDtoWithAnsw(from, size, userId);
     }
 
     @GetMapping("/{requestId}")
-    public ItemRequestDtoWithAnswer getRequestById(@RequestHeader ("X-Sharer-User-Id") int userId,
-                                                   @PathVariable int requestId ) {
+    public ItemRequestDtoWithAnswer getRequestById(@RequestHeader("X-Sharer-User-Id") int userId,
+                                                   @PathVariable int requestId) {
         log.info("Получен запрос c id = {}", requestId);
         return requestService.getDtoWithAnswById(requestId, userId);
     }
